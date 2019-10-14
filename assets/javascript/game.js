373cabd95fd351e6ecb6a711979d78e83ab5c4c0
Lucas Aho <aholucas11@gmail.com>
2019-10-14 16:00:21 -0600

    Added word array and code that will pick a random element and determine it's length

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//Possible words for player to guess
+var wordOptions = ["Mario","Doom","Overwatch","Tetris","Minecraft","Pacman","Halo"];
+
+//Create math function to randomly choose element
+var playerWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+console.log(playerWord);
+
+//Checks how many letter spaces to place on page
+var wordLength = playerWord.length
+console.log(wordLength);
+
+
+//Create a onkey capture
+
+//Create pool of guessed letters
+
+//Create win counter 
+
+//Creat
