ac46e4275188d0f0606aa4ef3a269ea759b559c9
Lucas Aho <aholucas11@gmail.com>
2019-10-14 18:40:16 -0600

    All basic elements complete, but a lot of debugging and refining necessary

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,78 @@
 //Possible words for player to guess
 var wordOptions = ["Mario","Doom","Overwatch","Tetris","Minecraft","Pacman","Halo"];
 
-//Create math function to randomly choose element
+//Variables for default game values
+var wins = 0;
+var guessCount = 15;
+
+//Create math function to randomly choose element and find it's length
 var playerWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
-console.log(playerWord);
 
-//Checks how many letter spaces to place on page
-var wordLength = playerWord.length
-console.log(wordLength);
+//Displaying the spaces for chosen word
+var lengthDisplay = [];
+    for (var i = 0; i < playerWord.length; i++) {
+        
+        lengthDisplay[i] = "_";
+    }
+    var underscoreWrite = document.getElementById('underscore-text');
+    underscoreWrite.innerHTML = lengthDisplay.join(" ");
+
+//Creates the pool of wrong guesses and takes off from the counter
+var guessWrong = [];
+
+var countWrite = document.getElementById('guessCount-text');
+    countWrite.innerHTML = guessCount;
+
+//Creates the win counter and updates it with wins
+var winWrite = document.getElementById('winCount-text');
+    winWrite.innerHTML = wins;
+
+//This runs when the player presses a key
+document.onkeyup = function(event) {
+    
+    //Determines which key was pressed
+    var playerGuess = event.key;
+
+    for (var j=0; j < playerWord.length; j++){
+        
+        if (playerWord[j]===playerGuess){
+            lengthDisplay[j]=playerGuess;
+        }
+        if (playerGuess !== playerWord[j]){
+            guessWrong.push(playerGuess);
+            guessCount --;
+            var wrongWrite = document.getElementById('guessWrong-text');
+            wrongWrite.innerHTML = guessWrong;
+        }
+        if (lengthDisplay == playerWord){
+            alert("You win!");
+            wins++;
+        }
+        if (guessCount===0){
+            alert("You lost, try again!");
+        }
+        var underscoreWrite = document.getElementById('underscore-text');
+            underscoreWrite.innerHTML = lengthDisplay.join(" ");
+            
+        var countWrite = document.getElementById('guessCount-text');
+            countWrite.innerHTML = guessCount;
+    }
 
 
-//Create a onkey capture
+    
+}
+
+
+
+
+//var winCount = document.getElementById("winCount-text");
+//var guessCount = document.getElementById("guessCount-text");
+
+//var wins= 0;
+
+
+
+
 
 //Create pool of guessed letters
 
-//Create win counter 
-
-//Creat
